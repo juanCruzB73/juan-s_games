01a243d0e34711fb6423f56d13b9d04d1c83dcdc
juanB <juancruzberrios73@gmail.com>
2024-03-02 12:46:58 -0300

    tetris funcionando

--- prueba.js
+++ prueba.js
@@ -10,10 +10,8 @@ console.log(a)
 let c=[]
 for(let i=0;i<5;i++){
     let d=[]
-    for(let j=0;j<5;j++){
+    for(let j=4;j>=0;j--){
         d.push(a[j][i])
     }
     c.push(d)
 }
-
-console.log(c)
--- tetris/main.js
+++ tetris/main.js
@@ -2,6 +2,9 @@ let canvas = document.querySelector('canvas');
 
 let context=canvas.getContext('2d');
 
+let score=document.getElementById('score')
+
+let currentScore=0;
 canvas.width=300
 canvas.height=600
 let startGame=false;
@@ -47,18 +50,17 @@ let figure7=[
 
 //event listeners
 move=''
-document.addEventListener("keydown",(event)=>{
-    event.preventDefault()
-    move=event.key
-    console.log(move)
-    homeScreen()
-})
+document.addEventListener("keydown", (event) => {
+    event.preventDefault();
+    move = event.key;
+    homeScreen();
+});
 
 document.addEventListener("keyup",(event)=>{
     event.preventDefault()
     move='';
 
-}) 
+})
 
 let columns=10;
 let rows=20;
@@ -90,7 +92,7 @@ let drawBoard=()=>{
 
 //figures
 let figureX=3
-let figureY=0
+let figureY=-1
 
 function drawSquare(x,y,color){
     context.fillStyle = color;
@@ -121,7 +123,7 @@ let unDrawFigure = (x,y,figure) => {
     }
 }
 //check for collition
-
+let changes=0
 function Game(x,y,figures){
     this.x=x;
     this.y=y;
@@ -134,31 +136,10 @@ function Game(x,y,figures){
         return [pieces[active][0],pieces[active][1]]
     }
     this.activeFigure=this.newFigure(this.figures)
-
-    //calculate widith of current figure
-    
-        //calculate maximun height of piece
-        
-        this.figureHeight=(figure)=>{
-            maximunFigureHeight=0;
-            for(let i=0;i<figure.length;i++){
-                for(let j=0;j<figure[i].length;j++){
-                    if(figure[i][j]==1){
-                        this.maximunFigureHeight++
-                        break;
-                    }
-                }
-
-            }
-            return this.maximunFigureHeight
-        }
-        this.maximunFigureHeight=this.figureHeight(this.activeFigure[0])
-
         //figure down per second
         this.downPerSecond=()=>{   
             if(this.collision(0,1,this.activeFigure[0])){ 
                     this.drawOldFigure(this.activeFigure[0],this.activeFigure[1])
-                    console.log("popo downSecond")
                     this.startOver()}
             this.y += 1;
             
@@ -176,14 +157,7 @@ function Game(x,y,figures){
                         }
                         
                         let nextX=this.x+j+x;
-                        let nextY=this.y+i+y;
-    
-                        /*if(nextY <= 0 || nextX <= 0){
-                            continue;
-                        }*/
-                        /*if () {
-                            return true; // Collision with button
-                        }*/
+                        
                         if ( nextX < 0 || nextX >= columns) {
                             return true; // Collision with boundaries
                         }
@@ -203,7 +177,7 @@ function Game(x,y,figures){
                     let nextX=this.x+j+x;
                     let nextY=this.y+i+y;
 
-                    if(nextY <= 0 || nextX <= 0){
+                    if(nextY <= 0 ){
                         continue;
                     }
                     if (nextY < 0 ||  nextY >= rows) {
@@ -226,86 +200,111 @@ function Game(x,y,figures){
                     }
                 }
             }
+            for(let i=0;i<rows;i++){
+                let fullRow=true;
+                for(let j=0;j<columns;j++){
+                    fullRow=fullRow && (board[i][j]!=notOccupied)
+                }
+                if(fullRow){
+                    currentScore+=100
+                    for(let k=i;k>1;k--){
+                        for(let l=0;l<columns;l++){
+                            board[k][l]=board[k-1][l]
+                        }
+                    }
+                    for(let m=0;m<columns;m++){
+                        board[0][m]=notOccupied
+                    }
+                }
+            }
         }
         //start over
         this.startOver=()=>{
                 this.x=x;
                 this.y=y;
                 this.activeFigure=this.newFigure(this.figures)
-                //this.maximunFigureHeight=0;
-                //this.figureHeight(this.activeFigure[0])
-                //this.FigureWidith(this.activeFigure[0]);
         }
         //rotate pieces
+        
         this.rotatePiece=(figure)=>{
             let newFigure=[]
-            for(let i=0;i<figure.length;i++){
-                let element=[]
-                for(let j=0;j<figure[i].length;j++){
-                    element.push(figure[j][i])
+            if(changes%2==0){
+                for(let i=figure.length-1;i>=0;i--){
+                    let element=[]
+                    for(let j=figure[i].length-1;j>=0;j--){
+                        element.push(figure[j][i])
+                    }
+                    newFigure.push(element)
                 }
-                newFigure.push(element)
+            }else{
+                for(let i=0;i<figure.length;i++){
+                    let element=[]
+                    for(let j=0;j<figure[i].length;j++){
+                        element.push(figure[j][i])
+                    }
+                    newFigure.push(element)
+                }
             }
-            if(!this.collision(0,0,newFigure)){
+            if(changes<4){
+                changes++
+            }else{
+                changes=0;
+            }
+            if(!this.collisionBorders(0,0,newFigure) && (!this.collision(0,0,newFigure))){
                 return newFigure
             }
+            return figure
         }
         //player moves
         switch (move){
             case "ArrowUp":
                 this.activeFigure[0]=this.rotatePiece(this.activeFigure[0])
-                this.FigureWidith(this.activeFigure[0])
+                console.log(changes)
+                move='';
                 break;
             case "ArrowDown":
-                if(this.collision(0,1,this.activeFigure[0])){
-                    this.drawOldFigure(this.activeFigure[0],this.activeFigure[1])
-                    console.log("popo arrow down")
-                    this.startOver()
+                if(!this.collision(0,1,this.activeFigure[0])){
+                    this.y += 1;
+                    move='';
                 }
-                this.y += 1;
                 break;
             case "ArrowRight":
-                if(this.collision(0,-1,this.activeFigure[0])){
-                    this.drawOldFigure(this.activeFigure[0],this.activeFigure[1]);
-                    console.log("popo arrow right")
-                    this.startOver();
-                    
-                }else if(!this.collisionBorders(1,0,this.activeFigure[0])){
+                if(!this.collisionBorders(1,0,this.activeFigure[0])&&!this.collision(1,0,this.activeFigure[0])){
                     this.x++;
+                    move='';
                 }
-                /*if(this.collision(1,0,this.activeFigure[0])){
-                    this.drawOldFigure(this.activeFigure[0],this.activeFigure[1]);
-                    this.startOver();
-                }else{
-                    this.x++;
-                }*/
                 break;
             case "ArrowLeft":
-                if(this.collision(0,-1,this.activeFigure[0])){
-                    this.drawOldFigure(this.activeFigure[0],this.activeFigure[1]);
-                    console.log("popo arrow right")
-                    this.startOver();
-                }else if(!this.collisionBorders(-1,0,this.activeFigure[0])){
+                if(!this.collisionBorders(-1,0,this.activeFigure[0])&&!this.collision(-1,0,this.activeFigure[0])){
                     this.x--
+                    move='';
                 }
                 break;
             case "Enter":
                 this.startOver()
+                setBoard(board)
+                break;
+            case "Escape":
+                startGame=false
+                break
+            case " ":
+                while(!this.collision(0,1,this.activeFigure[0])){
+                    this.y++
+                } 
                 break;
         }
-        if(this.y<0 && (this.collision(0,0,this.activeFigure[0]))){ 
+        if(this.y<=0 && (this.collision(0,0,this.activeFigure[0]))){ 
             setBoard(board);
             unDrawFigure(this.x,this.y,this.activeFigure[0]);
-            console.log("coñooo aaa")
+            looser=true;
             startGame=false
         }else{
             drawBoard()
-            console.log("coñooo")
             drawFigure(this.x,this.y,this.activeFigure[0],this.activeFigure[1])
         }
+        score.innerHTML=currentScore
     }
 }
-
 let figures=[[figure1,"yellow"],[figure2,"green"],[figure3,"red"],[figure4,"purple"],[figure5,"blue"],[figure6,"pink"],[figure7,"orange"]];
 
 let program = new Game(figureX,figureY,figures)
@@ -320,10 +319,8 @@ let homeScreen=()=>{
 function animate(){
     if(startGame){
         requestAnimationFrame(animate);
-    
         program.update()
     }else{
-        looser=true;
         init()
     }
     

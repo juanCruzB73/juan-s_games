f65e8fba29657a05ff10a2f39bcb753fbcab155e
juanB <juancruzberrios73@gmail.com>
2024-03-08 00:02:11 -0300

    serpiente funcionando

--- snake_game/main.js
+++ snake_game/main.js
@@ -3,6 +3,12 @@ let context = canvas.getContext('2d');
 canvas.width=innerWidth/1.5;
 canvas.height=innerHeight/1.2;
 
+let size=20;
+let snakeX=100;
+let snakeY=100;
+let lastFrameTime = 0;
+let frameDelay = 200;
+
 context.beginPath();
 
 let move=''
@@ -11,12 +17,8 @@ document.addEventListener("keydown",(event)=>{
     move = event.key;
     console.log(move)
 })
-document.addEventListener("keyup",(event)=>{
-    event.preventDefault();
-    move = '';
-    console.log(move)
-})
-let size=20;
+
+
 function Game(snakeX,snakeY){
     this.snakeX=snakeX;
     this.snakeY=snakeY;
@@ -28,29 +30,16 @@ function Game(snakeX,snakeY){
         context.strokeRect(this.snakeX,this.snakeY,size,size)
     }
 
-    this.update=()=>{
-        switch(move){
-            case "ArrowUp":
-                this.snakeY-=20;
-                break;
-            case "ArrowDown":
-                this.snakeY+=20
-                break;
-            case "ArrowRight":
-                this.snakeX+=20
-                break;
-            case "ArrowLeft":
-                this.snakeX-=20;
-                break;
-        }
+    this.update=(x,y)=>{
+          this.snakeX=x;
+          this.snakeY=y  
     }
 }
 let snake=[]
 let long=5;
+//let snakePart=''
 
 function createSnake(){
-    let snakeX=100;
-    let snakeY=100;
     for(let i=0;i<long;i++){
         let snakePiece = new Game(snakeX,snakeY);
         snake.push(snakePiece)
@@ -58,13 +47,38 @@ function createSnake(){
     }
 }
 createSnake()
-function animate(){
-    requestAnimationFrame(animate);
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    for(let i=0;i<long;i++){
-        snake[i].update();
-        snake[i].draw()
+
+function moveSnake(){
+    let headX=snake[0].snakeX;
+    let headY=snake[0].snakeY;
+    switch(move){
+        case "ArrowUp":
+            headY-=size;
+            break;
+        case "ArrowDown":
+            headY+=size;
+            break;
+        case "ArrowLeft":
+            headX-=size;
+            break
+        case "ArrowRight":
+            headX+=size;
+            break;
     }
+    for(let i=snake.length-1;i>0;i--){
+        snake[i].update(snake[i-1].snakeX,snake[i-1].snakeY);
+    }
+    snake[0].update(headX,headY);
+}
+function animate(){
+    
+    setInterval(()=>{
+        context.clearRect(0,0,canvas.width,canvas.height);
+        moveSnake();
+        for(let i=0;i<snake.length;i++){
+            snake[i].draw()
+        }
+    },100)
     
 }
 animate()
